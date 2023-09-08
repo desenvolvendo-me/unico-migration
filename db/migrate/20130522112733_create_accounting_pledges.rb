5125fdf83321d47a210ba93d5a64a2ee0527f1e5
Marco Castro <marcodotcastro@gmail.com>
2023-09-08 15:52:56 -0300

    build: add migration folha dependence

--- db/migrate/20130522112733_create_accounting_pledges.rb
+++ db/migrate/20130522112733_create_accounting_pledges.rb
@@ -1,39 +1,41 @@
 class CreateAccountingPledges < ActiveRecord::Migration[6.1]
   def change
-    create_table "accounting_pledges" do |t|
-      t.integer  "management_unit_id"
-      t.date     "emission_date"
-      t.integer  "budget_allocation_id"
-      t.decimal  "value",                    :precision => 10, :scale => 2
-      t.integer  "pledge_category_id"
-      t.datetime "created_at",                                              :null => false
-      t.datetime "updated_at",                                              :null => false
-      t.integer  "expense_kind_id"
-      t.integer  "accounting_historic_id"
-      t.integer  "contract_id"
-      t.text     "description"
-      t.integer  "reserve_fund_id"
-      t.string   "material_kind"
-      t.integer  "founded_debt_contract_id"
-      t.string   "pledge_modality"
-      t.integer  "licitation_process_id"
-      t.integer  "code"
-      t.integer  "creditor_id"
-      t.integer  "expense_nature_id"
-      t.integer  "descriptor_id"
-      t.integer  "precatory_id"
-      t.integer  "modality"
-      t.integer  "special_inscription_id"
-      t.hstore   "custom_data"
-      t.string   "licitation_process"
-      t.integer  "licitation_process_year"
-      t.integer  "capability_id"
-      t.integer  "main_pledge_id"
-      t.boolean  "leftover_pledge"
-      t.integer  "year"
-      t.decimal  "initial_value_not_processed",  :precision => 10, :scale => 2
-      t.decimal  "initial_value_processed",  :precision => 10, :scale => 2
-      t.text     "historic_complement"
+    unless table_exists? :accounting_pledges
+      create_table "accounting_pledges" do |t|
+        t.integer "management_unit_id"
+        t.date "emission_date"
+        t.integer "budget_allocation_id"
+        t.decimal "value", :precision => 10, :scale => 2
+        t.integer "pledge_category_id"
+        t.datetime "created_at", :null => false
+        t.datetime "updated_at", :null => false
+        t.integer "expense_kind_id"
+        t.integer "accounting_historic_id"
+        t.integer "contract_id"
+        t.text "description"
+        t.integer "reserve_fund_id"
+        t.string "material_kind"
+        t.integer "founded_debt_contract_id"
+        t.string "pledge_modality"
+        t.integer "licitation_process_id"
+        t.integer "code"
+        t.integer "creditor_id"
+        t.integer "expense_nature_id"
+        t.integer "descriptor_id"
+        t.integer "precatory_id"
+        t.integer "modality"
+        t.integer "special_inscription_id"
+        t.hstore "custom_data"
+        t.string "licitation_process"
+        t.integer "licitation_process_year"
+        t.integer "capability_id"
+        t.integer "main_pledge_id"
+        t.boolean "leftover_pledge"
+        t.integer "year"
+        t.decimal "initial_value_not_processed", :precision => 10, :scale => 2
+        t.decimal "initial_value_processed", :precision => 10, :scale => 2
+        t.text "historic_complement"
+      end
     end
   end
 end
